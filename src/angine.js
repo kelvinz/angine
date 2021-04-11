ee71323948d2fd9421b33a20618bfea941e94943
kelvin zhao <takingovertheworld@gmail.com>
2021-04-11 20:25:29 +0800

    🙏🏻 0.1.0
    commit de88599b7461de78fa75fd381711a4fa9cb6485f
    Author: kelvin zhao <takingovertheworld@gmail.com>
    Date:   Sun Apr 11 20:25:05 2021 +0800
    
        ✨ stop true
    
    commit 051de882bb78a009c8bbe540a4ad058ad1219b8d
    Author: kelvin zhao <takingovertheworld@gmail.com>
    Date:   Sun Apr 11 13:53:13 2021 +0800
    
        ✨ ngInit 🔥 ng, ngAdd, ngStart
    
    commit 43141c1195dba7496482f6b5f203838fe7304e80
    Author: kelvin zhao <takingovertheworld@gmail.com>
    Date:   Sun Apr 11 10:02:14 2021 +0800
    
        ✨ ng base

---
+++ src/angine.js
@@ -0,0 +1,42 @@
+let request, interval, now, then, elapsed
+let stop, pause
+
+const ng = {}
+const ngFns = []
+
+const animate = () => ngFns.forEach( fn => fn( ng ) )
+
+const loop = () => {
+	if ( !stop ) request = requestAnimationFrame( loop )
+	now = Date.now()
+	elapsed = now - then
+
+	if ( elapsed > interval ) {
+		then = now - ( elapsed % interval )
+		if ( !pause ) animate()
+	}
+}
+
+const play = fps => {
+	interval = 1000 / fps
+	then = Date.now()
+	loop()
+}
+
+export const ngInit = ( obj, fn, fps = 60 ) => {
+	ngStop()
+	Object.assign( ng, obj )
+	ngFns[ 0 ] = fn
+	stop = false
+	pause = false
+	play( fps )
+}
+
+export const ngStop = () => {
+	stop = true
+	ngFns.length = 0
+	if ( request ) cancelAnimationFrame( request )
+}
+
+export const ngPause = () => pause = true
+export const ngPlay = () => pause = false
--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 import './app.css'
+import { ngInit, ngPause, ngPlay, ngStop } from './angine'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const initNg = () => {
+	ngInit( {
+		timer: 0,
+		counter: 0,
+	}, ( state ) => {
+		state.counter++
+		if ( state.counter >= 60 ) {
+			state.timer++
+			state.counter = 0
+			console.log( state.timer )
+		}
+	} )
+}
+
+document.querySelector( '#start' ).addEventListener( 'click', () => initNg() )
+document.querySelector( '#pause' ).addEventListener( 'click', () => ngPause() )
+document.querySelector( '#play' ).addEventListener( 'click', () => ngPlay() )
+document.querySelector( '#stop' ).addEventListener( 'click', () => ngStop() )
